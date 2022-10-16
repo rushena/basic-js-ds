cabe99e88910dff766120a93fe84283286187cf1
Rushen Adjigeldieva <rushena2706@gmail.com>
2022-10-16 17:39:02 +0500

    feat: add binary search tree solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,136 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+    constructor() {
+        this.head = null;
+    }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+        return this.head;
   }z
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+      this.head = addData(this.head, data);
+
+      function addData(node, data) {
+          if (!node) return new Node(data);
+
+          if (node.data === data) return node;
+
+          if (node.data < data) {
+              node.right = addData(node.right, data);
+          }
+
+          if (node.data > data) {
+              node.left = addData(node.left, data);
+          }
+
+          return node;
+      }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+      const searchData = node => {
+          if (!node) return false;
+
+          if (node.data === data) return true;
+
+          if (node.data > data) return searchData(node.left);
+
+          if (node.data < data) return searchData(node.right);
+      }
+
+      return searchData(this.head);
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+      const findData = node => {
+          if (!node) return null;
+
+          if (node.data === data) return node;
+
+          if (node.data > data) return findData(node.left);
+
+          if (node.data < data) return findData(node.right);
+      }
+
+      return findData(this.head);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+      this.head = removeData(this.head);
+
+      function removeData(node) {
+
+          if (!node) return null;
+
+          if (node.data > data) {
+              node.left = removeData(node.left);
+
+              return node;
+          } else if (node.data < data) {
+              node.right = removeData(node.right);
+
+              return node;
+          } else {
+              if (!node.left && !node.right) return null;
+
+              if (!node.left) {
+                  node = node.right;
+                  return node;
+              }
+
+              if (!node.right) {
+                  node = node.left;
+                  return node;
+              }
+
+              let minFromRight = node.right;
+
+              while(minFromRight.left) {
+                  minFromRight = minFromRight.left;
+              }
+
+              node.data = minFromRight.data;
+              node.right = removeData(node.right, node.data);
+              return node;
+          }
+      }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      const findMin = node => {
+          if (!node) return null;
+
+          if (!node.left) {
+              return node.data;
+          }
+
+          return findMin(node.left);
+      }
+
+      return findMin(this.head);
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      const findMax = node => {
+          if (!node) return null;
+
+          if (!node.right) {
+              return node.data;
+          }
+
+          return findMax(node.right);
+      }
+
+      return findMax(this.head);
+
   }
 }
 
